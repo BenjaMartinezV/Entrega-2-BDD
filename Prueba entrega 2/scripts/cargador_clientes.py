8a4c0402a529e60837bc96f1efb1ac0b87c620a7
TomasArancibia <tomasarancibia@uc.cl>
2024-06-02 22:59:30 -0400

    Arreglo errores Clientes

--- Prueba entrega 2/scripts/cargador_clientes.py
+++ Prueba entrega 2/scripts/cargador_clientes.py
@@ -2,12 +2,14 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2 import Error
 
+
 DB_HOST = 'pavlov.ing.puc.cl'
 DB_PORT = '5432'
 DB_USER = 'grupo64'
 DB_PASSWORD = 'grupo64'
 DB_NAME = 'grupo64e2'
 archivo_csv = '../datos e2/pedidos2.csv'
+
 
 # Ruta del archivo CSV
 archivo_csv = '../datos e2/clientes.csv'
@@ -55,6 +57,7 @@ try:
     with open(archivo_csv, 'r') as file:
         lista_clientes = []
         cliente_actual = ""
+
         next(file)
         
         for linea in file:
@@ -81,10 +84,84 @@ try:
 
     fetch_cleaned_data_query = "SELECT * FROM Clientes"
     cursor.execute(fetch_cleaned_data_query)
+    clientes_all = cursor.fetchall()
+
+    def encriptar(clave, shift=3):
+            encrypted = []
+            for char in clave:
+                if char.isalpha():
+                    shift_amount = 65 if char.isupper() else 97
+                    encrypted.append(chr((ord(char) + shift - shift_amount) % 26 + shift_amount))
+                else:
+                    encrypted.append(char)
+            return ''.join(encrypted)
+    
+    def decriptar(clave, shift=3):
+        decrypted = []
+        for char in clave:
+            if char.isalpha():
+                shift_amount = 65 if char.isupper() else 97
+                decrypted.append(chr((ord(char) - shift - shift_amount) % 26 + shift_amount))
+            else:
+                decrypted.append(char)
+        return ''.join(decrypted)
+
+    for cliente in clientes_all:
+        id, nombre, email, telefono, clave, direccion, comuna = cliente
+        
+        enc_clave= encriptar(clave)
+
+        # Update the record with cleaned data and encrypted clave
+        update_query = """
+        UPDATE Clientes
+        SET clave = %s
+        WHERE id = %s;
+        """
+        cursor.execute(update_query, (enc_clave, id))
+    conexion.commit()
+
+
+    update_pablo_query = """
+    UPDATE Clientes
+    SET nombre = REPLACE(nombre, '"', '')
+    WHERE nombre LIKE '%"%';
+    """
+    cursor.execute(update_pablo_query)
+    conexion.commit()
+
+    update_nunoa_query = """
+    UPDATE Clientes
+    SET direccion = REPLACE(direccion, 'Nunoa', 'Ñuñoa')
+    WHERE direccion LIKE '%Nunoa%';
+    """
+    cursor.execute(update_nunoa_query)
+    conexion.commit()
+
+    update_nunoa_query = """
+    UPDATE Clientes
+    SET direccion = REPLACE(direccion, 'Vina', 'Viña')
+    WHERE direccion LIKE '%Vina%';
+    """
+    cursor.execute(update_nunoa_query)
+    conexion.commit()
+    
+
+    update_telefono_query = """
+    UPDATE Clientes
+    SET telefono = '+' || telefono;
+    """
+    cursor.execute(update_telefono_query)
+    conexion.commit()
+
+    print("datalimpia")
+    fetch_cleaned_data_query = "SELECT * FROM Clientes"
+    cursor.execute(fetch_cleaned_data_query)
     cleaned_data = cursor.fetchall()
     
     for row in cleaned_data:
         print(row)
+    
+
 
 except Error as e:
     print(f"Error de conexión o durante la operación en la base de datos: {e}")
@@ -95,5 +172,3 @@ finally:
         cursor.close()
     if conexion:
         conexion.close()
-
-       
